7fb428a755e524f0954c85e05a4b0c5e7715bfd5
Paul Sydney Orozco <xtrycatchx@gmail.com>
2017-12-02 17:19:17 +0800

    gcp x twilio experiment

---
+++ index.js
@@ -0,0 +1,9 @@
+'use strict';
+const Sms = require('./sms')
+
+exports.sms = (request, response) => {
+  const sms = new Sms();
+  sms.send(request.body.message,request.body.to).then(smsReceipt=>{
+    response.status(200).send(JSON.stringify(smsReceipt));
+  })
+};
