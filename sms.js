5d5bbd3b1f1fab7842f83118edbab4b55504f073
Paul Sydney Orozco <xtrycatchx@gmail.com>
2017-12-02 17:19:55 +0800

    gcp x twilio experiment

---
+++ sms.js
@@ -0,0 +1,19 @@
+const client = require('twilio')
+const config = require('./twilio.config.json')
+
+class Sms {
+    constructor() {
+        this._client = client(config.twilioAccounSID, config.twilioAuthToken)
+    }
+
+    send(message,to) {
+        return this._client.messages.create({
+            from: config.twilioDeveloperPhone,
+            to: to,
+            body: message
+        }).then(receipt => Promise.resolve({ sent: receipt.sid }))
+    }
+
+}
+
+module.exports = Sms;
